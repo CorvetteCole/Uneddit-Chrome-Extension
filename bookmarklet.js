34001345fcc73620348cb561a457e2e5783e92b0
Cole Gerdemann <corvettecole@gmail.com>
2018-04-21 20:35:06 -0400

    Updated to use Removeddit

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-void((function(){var e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src', '//uneddit.com/phase_one');document.body.appendChild(e)})());
+ document.location = document.URL.replace('reddit.com','removeddit.com');
